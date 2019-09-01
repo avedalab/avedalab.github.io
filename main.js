e443e4a68037da8da68d8a87d5b9cc52cbae1a0a
Baladeva Juganas <baladevajuganas@Baladevas-MacBook-Pro.local>
2019-09-01 17:14:15 +0800

    add info window and google analytics

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
             center: {lat: 10.31672, lng: 123.89071},
             zoom: 16
         });
-        addmarker({lat: 10.318487004786979, lng: 123.89610528945923}); // Ricos Lechon
-        addmarker({lat: 10.317558135101828, lng: 123.89408826828003}); // Zubuchon
-        addmarker({lat: 10.31506706195805, lng: 123.88988256454468}); // Yaksi Barbeque
-        addmarker({lat: 10.318233676962913, lng: 123.8866639137268}); // Planet Vegis
+
+        addmarker("Ricos Lechon", {lat: 10.318487004786979, lng: 123.89610528945923}, "PriChon, Camaro Rebosado, Crispy Feetchon");
+        addmarker("Zubuchon",{lat: 10.317558135101828, lng: 123.89408826828003}, "Zubuchon Sisig, Bangus Sisig, Chorizo Lumpia");
+        addmarker("Yaksi Barbeque",{lat: 10.31506706195805, lng: 123.88988256454468}, "Pork Barbeque, Chicken Leg, Tuna Belly"); 
+        addmarker("Planet Vegis",{lat: 10.318233676962913, lng: 123.8866639137268}, "BBQ, Chili Cheese Rocket, Chicharon Bean Curd"); 
 
         // google.maps.event.addListener(map, 'click', function( event ){
         //     alert( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
@@ -19,11 +20,29 @@ document.addEventListener("DOMContentLoaded", () => {
           
     });
     
-    function addmarker(coords)
+    function addmarker(title, coords, menu)
     {
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">'+title+'</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>'+title+'</b>, Menu : '+menu+'</p>'+
+            '</div>'+
+            '</div>';
+        // set marker    
         let marker = new google.maps.Marker({
             position:coords,
-            map:map
+            map:map,
+            title:title
+        });
+        // add info window per store
+        let infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        // set window content per store
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
         });
     }
 
