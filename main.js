03a93b6521819972487764c49409094458c94172
Baladeva Juganas <baladevajuganas@Baladevas-MacBook-Pro.local>
2019-09-03 17:45:47 +0800

    add filter and circle on map

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   sc.addEventListener("load", () => {
     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 10.31672, lng: 123.89071 },
+      center: { lat: 10.31672, lng: 123.89271 },
       zoom: 16
     });
 
@@ -16,62 +16,94 @@ document.addEventListener("DOMContentLoaded", () => {
       "Ricos Lechon",
       { lat: 10.318487004786979, lng: 123.89610528945923 },
       "PriChon, Camaro Rebosado, Crispy Feetchon",
-      "Contemporary Casual"
+      "Contemporary Casual",
+      100
     );
     addmarker(
       "Zubuchon",
       { lat: 10.317558135101828, lng: 123.89408826828003 },
       "Zubuchon Sisig, Bangus Sisig, Chorizo Lumpia",
-      "Fast Casual"
+      "Fast Casual",
+      500
     );
     addmarker(
       "Yaksi Barbeque",
       { lat: 10.31506706195805, lng: 123.88988256454468 },
       "Pork Barbeque, Chicken Leg, Tuna Belly",
-      "Fast Casual"
+      "Fast Casual",
+      110
     );
     addmarker(
       "Planet Vegis",
       { lat: 10.318233676962913, lng: 123.8866639137268 },
       "BBQ, Chili Cheese Rocket, Chicharon Bean Curd",
-      "Family Style"
+      "Family Style",
+      89
     );
 
     addmarker(
       "Ice Giants",
       { lat: 10.315214838033565, lng: 123.8909125328064 },
       "Ice giants, Mexican tostada, Mango giants",
-      "Contemporary Casual"
+      "Contemporary Casual",
+      80
     );
 
     addmarker(
       "Jollibee",
       { lat: 10.31544705744066, lng: 123.8852047920227 },
       "Yum burger, Jolly Hotdog, Tuna Pie",
-      "Fast Food"
+      "Fast Food",
+      2000
     );
 
     addmarker(
       "Coffee Notes Caffe",
       { lat: 10.315594833337782, lng: 123.89466762542725 },
       "Choco Fraup, Coffee",
-      "Cafe"
+      "Cafe",
+      300
     );
 
     addmarker(
       "House of Lechon",
       { lat: 10.317748131396826, lng: 123.90170574188232 },
-      "Lechon Sisig, LEchon paksiw, barn-iBisaya",
-      "Contemporary Casual"
+      "Lechon Sisig, Lechon paksiw, barn-iBisaya",
+      "Contemporary Casual",
+      150
     );
 
     addmarker(
       "The Pig and Palm",
       { lat: 10.312892634541782, lng: 123.90449523925781 },
       "Roasted parrot fish, Confit pork belly, Beer battered Fish and chips",
-      "Fine Dining"
+      "Fine Dining",
+      50
     );
+    
+    // circle block
+    // First, create an object containing LatLng and population for each city.
+    var circlemap = {
+      cebu: {
+        center: { lat: 10.31672, lng: 123.89491 }
+      }
+    };
 
+    for (var city in circlemap) {
+      // Add the circle for this city to the map.
+      var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: circlemap[city].center,
+        radius: 400
+      });
+    }
+
+    // search block
     var defaultBounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(10.2833322, 123.8907089),
       new google.maps.LatLng(10.31672, 123.8999964)
@@ -153,7 +185,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // });
   });
 
-  function addmarker(title, coords, menu, type) {
+  function addmarker(title, coords, menu, type, no) {
     var contentString =
       '<div id="content">' +
       '<div id="siteNotice">' +
@@ -165,10 +197,11 @@ document.addEventListener("DOMContentLoaded", () => {
       "<p><b>Type: </b>" +
       type +
       "</p>" +
-      "<p><b>" +
-      title +
-      "</b>, Menu : " +
+      "<p> <b>Menu: </b>" +
       menu +
+      "</p>" +
+      "<p> <b>Customer Visited: </b>" +
+      no +
       "</p>" +
       "</div>" +
       "</div>";
